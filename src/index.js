2c50fefe9254f18d888dda5291f60fb6566465b1
Dipen Ahlawat <67955879+dipenahlawat@users.noreply.github.com>
2020-07-29 11:26:54 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'C:/Users/Administrator/Desktop/brainprodigy/brain-prodigy/src/index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
